4c98d1c3ad6d29420557aeb9d4bdc5b2c158280d
Pedro Sobral <Pedro.Marques.Sobral@formandos.cinel.pt>
2021-01-06 19:58:52 +0000

    inicializaçao do projeto

---
+++ Lojacet49f/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lojacet49f.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
